bc3d96a8a48a1afbf6cc7a09fb31065c9c3e31ed
Stephen Foster <stephenfoster@f0-b4-79-1d-44-d7.dynamic.ucsd.edu>
2011-12-09 12:50:06 -0800

    First Commit

---
+++ Assets/Euclid/Eclipse.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using System.Collections;
+
+
+/*
+ *  Why is this a MonoBehaviour when it's just a library?  So you can access it from a JavaScript program without having
+ *  to put this in StandardAssets.
+ * 
+ *  Why not put this class in StandardAssets?  Because it relies on the Shell Project, which is part of Standard Assets but is (currently)
+ *  written in JavaScript.  We can move this to Standard Assets when the Shell Project is changed.  But the Shell project is being used by
+ *  the June project.  So this will require some coordination.
+ * 
+ *  See the Example files for usage.
+ * 
+ **/
+public class Eclipse : MonoBehaviour {
+	
+	public string JavaSrcUpdateString(string file_name)
+	{
+		string messages=Shell.shell("bash","\""+EuclidConfig.eclipse_path+"/eclim\" --nailgun-port 9091 -command java_src_update -p \"Scrolls\" -f \"src/"+file_name+"\" -v");
+		return messages;	
+	}
+	
+	public string JavaCompleteString(string file_name, int cursor)
+	{
+		string messages=Shell.shell("bash","\""+EuclidConfig.eclipse_path+"/eclim\" --nailgun-port 9091 -command java_complete -p \"Scrolls\" -f \"src/"+file_name+"\" -o "+cursor+" -e utf-8 -l compact");
+		return messages;
+	}
+	
+	public string ProjectListString()
+	{
+		string messages=Shell.shell("bash","\""+EuclidConfig.eclipse_path+"/eclim\" --nailgun-port 9091 -command projects");
+		return messages;
+	}
+}
---
+++ Assets/Euclid/EuclidConfig.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System.Collections;
+
+public class EuclidConfig{
+	
+	//When you've installed Eclipse.
+	public static string eclipse_path = "/Users/stephenfoster/Desktop/Research/WitchDoctor2/eclipse";
+	
+	//The place where java code scrolls are stored.  We use Eclim to turn this into an Eclipse project.  This let's us leverage Eclipse support in-game.
+	public static string java_project_path = "/Users/stephenfoster/GUITesting/Assets/Resources/Scrolls";
+
+}
