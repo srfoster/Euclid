a78ad7908a8305d5c706aa407e8d2d1fc4eff701
Stephen Foster <stephenfoster@Stephen-Fosters-MacBook-Pro.local>
2011-12-10 09:16:10 -0800

    The 'Scrolls' project was hardcoded in Eclipse.cs

--- Assets/Euclid/Eclipse.cs
+++ Assets/Euclid/Eclipse.cs
@@ -15,15 +15,15 @@ using System.Collections;
  **/
 public class Eclipse : MonoBehaviour {
 	
-	public string JavaSrcUpdateString(string file_name)
+	public string JavaSrcUpdateString(string file_name, string project)
 	{
-		string messages=Shell.shell("bash","\""+EuclidConfig.eclipse_path+"/eclim\" --nailgun-port 9091 -command java_src_update -p \"Scrolls\" -f \"src/"+file_name+"\" -v");
+		string messages=Shell.shell("bash","\""+EuclidConfig.eclipse_path+"/eclim\" --nailgun-port 9091 -command java_src_update -p \""+project+"\" -f \"src/"+file_name+"\" -v");
 		return messages;	
 	}
 	
-	public string JavaCompleteString(string file_name, int cursor)
+	public string JavaCompleteString(string file_name, string project, int cursor)
 	{
-		string messages=Shell.shell("bash","\""+EuclidConfig.eclipse_path+"/eclim\" --nailgun-port 9091 -command java_complete -p \"Scrolls\" -f \"src/"+file_name+"\" -o "+cursor+" -e utf-8 -l compact");
+		string messages=Shell.shell("bash","\""+EuclidConfig.eclipse_path+"/eclim\" --nailgun-port 9091 -command java_complete -p \""+project+"\" -f \"src/"+file_name+"\" -o "+cursor+" -e utf-8 -l compact");
 		return messages;
 	}
 	
